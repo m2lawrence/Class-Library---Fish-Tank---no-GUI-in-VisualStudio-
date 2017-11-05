b1449edd3f4c831758865cb01dab3c6a733a3068
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2017-11-05 19:56:12 +0000

    Add files via upload

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            //build an object.
+            Tank Tank1 = new Tank();
+
+            //link method in object and write it.
+            //*A user should be able to see how much food to put in the tank with a Tank1.Feed().
+            //This should return the weight in grams of the total required fish food. 
+            Tank1.Feed();
+
+            //pause.
+            Console.ReadKey(true);
+        }
+
+    }
+}
---
+++ ClassLibrary2/Fish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace ClassLibrary2
+{
+    public class Fish
+    {
+        //build a class to store the fish's name.         
+            public string name { get; set; }
+    }
+}
